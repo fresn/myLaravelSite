a59eac84dcbfe8c60e7d77befa1f4cf5220e32d9
Ian Ma <emen.fresn.ma@gmail.com>
2019-08-24 11:06:37 -0500

    basic pages

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help',"StaticPagesController@help");
+Route::get('/about','StaticPagesController@about');
+
+
 Route::get('/env',function (){
     return dump($_ENV);
 });
